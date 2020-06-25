29dae85b710186a002eff715850cc2d9f1d7f033
Raffay Ahmed <raf.amed97@gmail.com>
2020-06-24 21:37:31 -0700

    updated js

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,5 +1,5 @@
-<<<<<<< HEAD
-=======
+var events = 0;
+
 $("#search").click(function () {
     let artistSearch = $("#artist").val();
     //console.log(artistSearch)
@@ -8,7 +8,7 @@ $("#search").click(function () {
 });
 
 function fmSearch(artistSearch){
-    console.log(artistSearch)
+    $("#artistName").text(artistSearch);
     artistSearch = encodeURIComponent(artistSearch.trim())
     console.log(artistSearch)
 
@@ -19,43 +19,8 @@ function fmSearch(artistSearch){
         url: queryURL1,
         method: "GET",
       }).then(function(response) {
-  
-        console.log(response);
-
         for(i = 0; i < 3; i++){
-          console.log(response.toptracks.track[i].name)
-          //$("ID").val(response.toptracks.track[i].name)
-        }
-      });
-
-//ticketmaster info will be displayed below spotify info
-
->>>>>>> master
-$("#search").click(function () {
-    let artistSearch = $("#artist").val();
-    //console.log(artistSearch)
-    fmSearch(artistSearch);
-    ticketSearch(artistSearch);
-});
-
-function fmSearch(artistSearch){
-    console.log(artistSearch)
-    artistSearch = encodeURIComponent(artistSearch.trim())
-    console.log(artistSearch)
-
-    //Searching for Top Songs
-    var queryURL1 = "http://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist=" + artistSearch + "&api_key=c480f7f0a05a2b8e0d17968d01265fbe&format=json"
-
-    $.ajax({
-        url: queryURL1,
-        method: "GET",
-      }).then(function(response) {
-  
-        console.log(response);
-
-        for(i = 0; i < 3; i++){
-          console.log(response.toptracks.track[i].name)
-          //$("ID").val(response.toptracks.track[i].name)
+          $("#songTitle" + i).text(response.toptracks.track[i].name)
         }
       });
 
@@ -66,6 +31,7 @@ function fmSearch(artistSearch){
       url: queryURL2,
       method: "GET",
     }).then(function(response) {
+      $(".subtitle").text(response.artist.tags.tag[0].name);
 
       console.log(response);
 
@@ -74,11 +40,8 @@ function fmSearch(artistSearch){
 
 function ticketSearch(artistSearch){
   var queryURL1 = "https://rest.bandsintown.com/artists/" + artistSearch + "/?app_id=45255e6cc480b6c589613047fb0d5749"
-<<<<<<< HEAD
-  var events = 0;
+
   
-=======
->>>>>>> master
   $.ajax({
     url: queryURL1,
     method: "GET",
@@ -91,9 +54,8 @@ function ticketSearch(artistSearch){
     events = response.upcoming_event_count
 
     //Artist Image
-    console.log(response.image_url)
-
-  })
+    $("#artistImg").attr("src", response.image_url)
+  
 
   var queryURL2 = "https://rest.bandsintown.com/artists/" + artistSearch + "/events/?app_id=45255e6cc480b6c589613047fb0d5749"
 
@@ -101,28 +63,31 @@ function ticketSearch(artistSearch){
     url: queryURL2,
     method: "GET",
   }).then(function(response){
-    //Response from API
-    console.log(response)
     console.log(events)
-
+    
     if(events > 0){
-          //Number of Events Coming Up
-    let date0 = new Date( Date.parse(response[0].datetime))
-    console.log(date0)
-    console.log(response[0].venue.location)
-
-    let date1 = new Date( Date.parse(response[1].datetime))
-    console.log(date1)
-    console.log(response[1].venue.location)
-
-    let date2 = new Date( Date.parse(response[2].datetime))
-    console.log(date2)
-    console.log(response[2].venue.location)
-    }
-    else{
-      
+      for(i = 0; i < 3; i++){
+        let date = new Date( Date.parse(response[i].datetime))
+        $("#time" + i).text(date)
+        $("#location" + i).text(response[i].venue.location)
+      }
+    }else{
+        for(i = 0; i < 3; i++){
+          $("#time" + i).text("No Upcoming Events")
+          $("#location" + i).text("")
+      }
     }
 
+    // let date0 = new Date( Date.parse(response[0].datetime))
+    // $("#time0").text(date0);
+    // $("#location0").text(response[0].venue.location);
 
-  })
+    // let date1 = new Date( Date.parse(response[1].datetime))
+    // $("#time1").text(date1);
+    // $("#location1").text(response[1].venue.location);
+
+    // let date2 = new Date( Date.parse(response[2].datetime))
+    // $("#time2").text(date2);
+    // $("#location2").text(response[2].venue.location);
+})})
 }
